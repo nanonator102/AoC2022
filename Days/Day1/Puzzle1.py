d3a78ecfe2791f73f85301ce87e13a2dc651341b
Zachary Moroney <thenanonator102@gmail.com>
2022-12-01 16:10:05 +1100

    Completion of day 1

--- Days/Day1/Puzzle1.py
+++ Days/Day1/Puzzle1.py
@@ -1,2 +1,14 @@
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        calories = 0
+        max_calories = 0
+        for line in fp:
+            line = str.strip(line)  # Strips the new-line character
+            if line == "":
+                if calories > max_calories:
+                    max_calories = calories
+                calories = 0
+            else:
+                calories += int(line)
+        print(max_calories)
+
--- Days/Day1/Puzzle2.py
+++ Days/Day1/Puzzle2.py
@@ -1,2 +1,24 @@
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        calories = 0
+        first_max_calories = 0
+        second_max_calories = 0
+        third_most_calories = 0
+        for line in fp:
+            line = str.strip(line)  # Strips the new-line character
+            if line == "":
+                if calories > third_most_calories:
+                    third_most_calories = calories
+                    if third_most_calories > second_max_calories:
+                        third_most_calories, second_max_calories = second_max_calories, third_most_calories
+                        if second_max_calories > first_max_calories:
+                            second_max_calories, first_max_calories = first_max_calories, second_max_calories
+                calories = 0
+            else:
+                calories += int(line)
+        max_calories = first_max_calories + second_max_calories + third_most_calories
+        print(first_max_calories)
+        print(second_max_calories)
+        print(third_most_calories)
+        print(max_calories)
+
