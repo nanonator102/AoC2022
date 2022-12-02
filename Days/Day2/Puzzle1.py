8ceacebf048a5d86c0ef73ad7695b0dc65b64b6c
Zachary Moroney <thenanonator102@gmail.com>
2022-12-02 17:54:11 +1100

    Completion of day 1

--- Days/Day2/Puzzle1.py
+++ Days/Day2/Puzzle1.py
@@ -1,2 +1,39 @@
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        score = 0
+        for line in fp:
+            line = str.strip(line)  # Strips the new-line character
+            moves = line.split()
+            # Add score for my move
+            # A, X = Rock
+            # B, Y = Paper
+            # C, Z = Scissors
+            match moves[1]:
+                case 'X':
+                    score += 1
+                    match moves[0]:
+                        case 'A':
+                            score += 3
+                        case 'B':
+                            pass
+                        case 'C':
+                            score += 6
+                case 'Y':
+                    score += 2
+                    match moves[0]:
+                        case 'A':
+                            score += 6
+                        case 'B':
+                            score += 3
+                        case 'C':
+                            pass
+                case 'Z':
+                    score += 3
+                    match moves[0]:
+                        case 'A':
+                            pass
+                        case 'B':
+                            score += 6
+                        case 'C':
+                            score += 3
+        print(score)
--- Days/Day2/Puzzle2.py
+++ Days/Day2/Puzzle2.py
@@ -1,2 +1,40 @@
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        score = 0
+        for line in fp:
+            line = str.strip(line)  # Strips the new-line character
+            moves = line.split()
+            # Add score for my move
+            # A = Rock, 1 point
+            # B = Paper, 2 points
+            # C = Scissors, 3 points
+            # X = Lose, 0 points
+            # Y = Draw, 3 points
+            # Z = Win, 6 points
+            match moves[1]:
+                case 'X':
+                    match moves[0]:
+                        case 'A':  # My move is Scissors
+                            score += 3
+                        case 'B':  # My move is Rock
+                            score += 1
+                        case 'C':  # My move is Paper
+                            score += 2
+                case 'Y':
+                    match moves[0]:
+                        case 'A':  # My move is Rock
+                            score += 4
+                        case 'B':  # My move is Paper
+                            score += 5
+                        case 'C':  # My move is Scissors
+                            score += 6
+                case 'Z':
+                    match moves[0]:
+                        case 'A':  # My move is Paper
+                            score += 8
+                        case 'B':  # My move is Scissors
+                            score += 9
+                        case 'C':  # My move is Rock
+                            score += 7
+            print(moves)
+        print(score)
