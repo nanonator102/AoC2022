58d1476e847a42632a869a5a93f41b57cc7dba3b
Zachary Moroney <thenanonator102@gmail.com>
2022-12-04 17:17:32 +1100

    Completed day 3.

--- Days/Day3/Puzzle1.py
+++ Days/Day3/Puzzle1.py
@@ -1,2 +1,22 @@
+UPPERCASE_OFFSET = 38
+LOWERCASE_OFFSET = 96
+
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        priority = 0
+        for line in fp:
+            line = str.strip(line)
+            first, second = line[:len(line)//2], line[len(line)//2:]
+            print(f'First: {first}, Second: {second}')
+            duplicate = None
+            for i in first:
+                for j in second:
+                    if i == j:
+                        duplicate = i
+            if duplicate is not None:
+                if duplicate.isupper():
+                    priority += (ord(duplicate)-UPPERCASE_OFFSET)
+                else:
+                    priority += (ord(duplicate)-LOWERCASE_OFFSET)
+                duplicate = None
+    print(priority)
--- Days/Day3/Puzzle2.py
+++ Days/Day3/Puzzle2.py
@@ -1,2 +1,30 @@
+UPPERCASE_OFFSET = 38
+LOWERCASE_OFFSET = 96
+
 if __name__ == '__main__':
-    pass
+    with open('input', 'r') as fp:
+        priority = 0
+        lines = []
+        for line in fp:
+            line = line.strip()
+            lines.append(line)
+            if len(lines) != 3:  # If 3 lines are not stored then continue to next loop.
+                continue
+            # Find badge between rucksacks
+            badge = None
+            for i in lines[0]:
+                for j in lines[1]:
+                    if i == j:
+                        for ij in lines[2]:
+                            if i == ij:
+                                badge = i
+                                break
+            # Convert identifier to number and add to priority
+            if badge is not None:
+                if badge.isupper():
+                    priority += (ord(badge)-UPPERCASE_OFFSET)
+                else:
+                    priority += (ord(badge)-LOWERCASE_OFFSET)
+                badge = None
+            lines.clear()
+    print(priority)
